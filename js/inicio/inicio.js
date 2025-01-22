5f7aa657e1724551bede4da623081c2861c1418c
CesarDev1310 <nmantillaq@gmail.com>
2025-01-22 08:37:16 -0500

    Clase 22 - 01

--- js/inicio/inicio.js
+++ js/inicio/inicio.js
@@ -1,7 +1,35 @@
 
+window.onload = function(){
+    //Todo lo que este acá se va a ejecutar cuando la 
+    //pagina este cargada
+
+    mostrarMensaje();
+}
 
 
-function mostrarMensaje(){
+//Variables
+const nombre = "César";
+let apellido = "Mantilla";
+var direccion = "Jr. Direccion 123";
+var edad = 30;
 
-    console.log("Vicente, escribe algun mensaje acá");
+let alumno1 = {
+    nombre:"César",
+    apellido: "Mantilla",
+    edad: 20,
+    direccion: "Jr. Direccion 123"
+}
+let alumno2 = {
+    nombre:"Vicente",
+    apellido: "Villena",
+    edad: 19,
+    direccion: "Jr. Direccion 456"
+}
+
+let arregloAlumno = [alumno1, alumno2];
+let arregloNumeros = [1, 2,3 , 4];
+let arregloX = [1, 2,3 , 4, "Hola", alumno1];
+
+function mostrarMensaje(valor){
+    alert("Hola "+valor);
 }
