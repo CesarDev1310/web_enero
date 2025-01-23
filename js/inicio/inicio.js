9651459303fcc01411073cf0deb32a8a717a4071
Cesar Mantilla <you@example.com>
2025-01-23 08:56:28 -0500

    Sesion 2025 23

--- js/inicio/inicio.js
+++ js/inicio/inicio.js
@@ -3,11 +3,52 @@ window.onload = function(){
     //Todo lo que este acá se va a ejecutar cuando la 
     //pagina este cargada
 
+    $("#idEnviar").attr("disabled", true);
     mostrarMensaje();
+
+    $("#idCheckbox").on("change", function(){   
+        let checked = v_confirmacion = $(this).prop("checked");
+        if (checked) {
+            $("#idEnviar").removeAttr("disabled", true)
+        }else{
+            $("#idEnviar").attr("disabled", true)
+        }
+    });
+
+    $("#idEnviar").on("click", function(){
+        enviarDatos();
+    });
+}
+
+function enviarDatos(){
+    v_nombre = $("#idNombre").val();
+    v_correo = $("#idEmail").val();
+    v_contrasenia = $("#idPassword").val();
+
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    let resultado = regex.test(v_correo);
+
+    if (v_confirmacion && 
+        v_nombre != null && v_nombre.trim().length > 0 &&
+        v_correo != null && v_correo.trim().length > 0 && resultado &&
+        v_contrasenia != null && v_contrasenia.trim().length > 0
+    ) {
+        $("#idNavbar").text(v_nombre);
+    }else{
+        $("#idNavbar").text("Navbar");
+        alert("Ingrese datos válidos");
+    }
+
 }
 
 
 //Variables
+let v_confirmacion = false;
+let v_nombre = null;
+let v_correo = null;
+let v_contrasenia = null;
+
+
 const nombre = "César";
 let apellido = "Mantilla";
 var direccion = "Jr. Direccion 123";
@@ -31,5 +72,6 @@ let arregloNumeros = [1, 2,3 , 4];
 let arregloX = [1, 2,3 , 4, "Hola", alumno1];
 
 function mostrarMensaje(valor){
-    alert("Hola "+valor);
+    console.log("Bienvenidos !!")
 }
+
